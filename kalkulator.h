c87c8616a4cd96531c3637d7ad49c78656619601
raizafahra <raizafahra@mhs.pelitabangsa.ac.id>
2018-12-01 21:22:30 +0700

    first commit

---
+++ kalkulator.h
@@ -0,0 +1,11 @@
+#ifndef KALKULATOR_H_INCLUDED
+#define KALKULATOR_H_INCLUDED
+
+int kali(int, int);
+double bagi(int, int);
+int tambah(int, int);
+int kurang(int, int);
+
+
+
+#endif // KALKULATOR_H_INCLUDED
